e748514a989d3fe1a8b426f749a67f31b2f94faa
Naleo <naleo@naleo.me>
2022-10-20 16:07:46 -1000

    Add route to show reflection

--- app/controllers/reflections_controller.rb
+++ app/controllers/reflections_controller.rb
@@ -1,6 +1,6 @@
 class ReflectionsController < ApplicationController
   before_action :load_gratitude_item
-
+  before_action :load_reflection, except: [:index, :new, :create]
 
   def index
   end
@@ -17,6 +17,9 @@ class ReflectionsController < ApplicationController
       render :new, status: :unprocessable_entity
     end
   end
+
+  def show
+  end
   
   
   private
@@ -24,9 +27,14 @@ class ReflectionsController < ApplicationController
   def reflection_params
     params.require(:reflection).permit(:body, :reflection_date)
   end
-  
+
   def load_gratitude_item
     @gratitude_item = GratitudeItem.find params[:gratitude_item_id]
   end
+
+  def load_reflection
+    @reflection = Reflection.find params[:id]
+  end
+
 
 end
