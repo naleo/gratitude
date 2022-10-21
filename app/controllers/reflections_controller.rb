e5c9d0c3bcde41b02c913ed8806bc223f1613b13
Naleo <naleo@naleo.me>
2022-10-20 15:48:48 -1000

    Add create reflection route

--- app/controllers/reflections_controller.rb
+++ app/controllers/reflections_controller.rb
@@ -9,7 +9,22 @@ class ReflectionsController < ApplicationController
     @reflection = Reflection.new
   end
   
+  def create
+    @reflection = Reflection.new reflection_params
+    if @gratitude_item.reflections << @reflection
+      redirect_to gratitude_item_reflection_path(@gratitude_item,@reflection), notice: "Reflection Created"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+  
+  
   private
+  
+  def reflection_params
+    params.require(:reflection).permit(:body, :reflection_date)
+  end
+  
   def load_gratitude_item
     @gratitude_item = GratitudeItem.find params[:gratitude_item_id]
   end
